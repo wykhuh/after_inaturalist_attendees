9ec90e5dd85da220bcfeb745aa20d833d317dd70
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-07-24 22:47:44 -0700

    update exercise files

--- scripts/exercises/2_creating-maps.R
+++ scripts/exercises/2_creating-maps.R
@@ -4,43 +4,44 @@ library(ggplot2) # create data visualizations
 library(sf) # handle vector geospatial data
 library(mapview) # create interactive maps
 library(here) # file paths
-source(here('scripts/map_utils.R'))
+source(here('scripts/data_utils.R'))
+
 
 # ## Exercise 1
-#
+# 
 # Create a map for one species.
-#
-# -   Use read_csv() to load the CNC CSV. Assign the results to `my_inat_data` object.
+# 
+# -   use `read_csv()` to read iNaturalist file. Assign the results to `my_inat_data` object.
 # -   use `st_as_sf()` to add `geometry` column.
-# -   use `filter()` to select observations for one species.
 # -   use `select()` to pick four columns.
+# -   use `filter()` to select observations for one species.
 # -   Assign the results of `filter()` and `select()` to `my_inat_sf`
 # -   create either a static or interactive map.
-
+# 
 
 
 # ## Exercise 2
-#
+# 
 # Create a map for one species with LA County boundary.
-#
+# 
 # -   use iNaturalist observations `my_inat_sf` from Exercise 1
 # -   use `read_sf()` to read LA County boundary
 # -   check if iNaturalist and LA County boundary use the same CRS
 # -   create either a static or interactive map.
-
+# 
 
 
 # ## Exercise 3
-#
+# 
 # Create a map for all observations that are inside of a specific area
-#
+# 
 # -   use `my_inat_data` from exercise 1 to access iNaturalist data
 # -   use `st_as_sf()` to add `geometry` column to iNaturalist data.
 # -   use `select()` to select 4 columns for iNaturalist data.
-# -   use [Draw map boundaries](https://wykhuh.github.io/draw-map-boundaries/)
-#     to draw and download an area that you are interested in.
+# -   use [Draw map boundaries](https://wykhuh.github.io/draw-map-boundaries/) to draw and download an area that you are interested in.
 # -   Save the file to the `data/raw` directory.
 # -   use `read_sf()` to read your boundary data.
 # -   check if iNaturalist observations and your boundary use the same CRS
-# -   get observations inside the boundary
+# -   get observations inside a boundary
 # -   create static or interactive map
+# 
--- scripts/exercises/3_creating-charts.R
+++ scripts/exercises/3_creating-charts.R
@@ -4,26 +4,26 @@ library(ggplot2) # create data visualizations
 library(lubridate) # manipulate dates
 library(here) # file paths
 
+
 # ## Exercise 1
-#
+# 
 # Create a chart with all your observations for each year
-#
+# 
 # -   Use `read_csv()` to read iNaturalist file.
-# -   Use `mutate()` to add year column
-# -   Use `filter()` to select observations you made. Use 'quantron' as the
-#     user if you don't have CNC observations.
-# -   Assign the results of `mutate()` and `filter()` to `my_obs_by_year`
+# -   Use `mutate()` and `year()` to add year column
+# -   Use `filter()` to select observations you made. Use 'quantron' as the user if you don't have CNC observations.
+# -   Save the data frame to `my_obs_by_year` object
 # -   Use `ggplot()` to set the data and aesthetics.
 # -   Choose which type of chart you want: bar or line.
-
+# 
 
 
 # ## Exercise 2: Customizing a plot
-#
-# Take the `my_obs_by_year` data frame from exercise 1 and create a plot.
-# Customize the appearance or the chart. Here are some ideas.
-#
+# 
+# Take the `my_obs_by_year` data frame from exercise 1 and create a plot. Customize the appearance or the chart. Here are some ideas.
+# 
 # -   add a title
 # -   choose the one of the built in themes
 # -   change the axis titles
 # -   change the colors of the bars or lines
+# 
